7c28e9ceb76e73781c922edc5a2849b8b9640630
Mostafa <moayedpour.m@gmail.com>
2022-03-14 10:50:25 +0330

    add actionSheet to work with flatList

--- src/components/swipeList.js
+++ src/components/swipeList.js
@@ -2,58 +2,263 @@ import React from 'react'
 import {
   Box,
   FlatList,
+  Avatar,
+  HStack,
+  VStack,
   Text,
+  Spacer,
+  Center,
+  View,
+  NativeBaseProvider,
   Actionsheet,
   useDisclose,
   Button,
 } from 'native-base'
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    paddingTop: 50,
+  },
+})
 
 export default function SwipeListe() {
   const data = [
     {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28babbbbzzzz',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28babbbzzz',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28bannzz',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28banz',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28babbbb',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28babbb',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28bann',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ban',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28bamm',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28bam',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28bapp',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28bap',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28b',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
       id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
       fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl:
+        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
     },
     {
       id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
       fullName: 'Sujitha Mathur',
+      timeStamp: '11:11 PM',
+      recentText: 'Cheer up, there!',
+      avatarUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU',
     },
     {
       id: '58694a0f-3da1-471f-bd96-145571e29d72',
       fullName: 'Anci Barroco',
+      timeStamp: '6:22 PM',
+      recentText: 'Good Day!',
+      avatarUrl: 'https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg',
     },
     {
       id: '68694a0f-3da1-431f-bd56-142371e29d72',
       fullName: 'Aniket Kumar',
+      timeStamp: '8:56 PM',
+      recentText: 'All the best',
+      avatarUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU',
     },
     {
       id: '28694a0f-3da1-471f-bd96-142456e29d72',
-      fullName: 'Kiara',
+      fullName: 'Kiara Aghel',
+      timeStamp: '12:47 PM',
+      recentText: 'I will call today.',
+      avatarUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU',
     },
   ]
+
+  const {isOpen, onOpen, onClose} = useDisclose()
+
   return (
-    <Box textAlign="center" bg="white">
-      <FlatList
-        data={data}
-        renderItem={({item}) => (
-          <Box
-            borderBottomWidth="1"
-            _dark={{
-              borderColor: 'gray.600',
-            }}
-            borderColor="coolGray.200">
-            <Text
+    <NativeBaseProvider>
+      <View style={styles.page}>
+        <Center mt={5} m="auto">
+          <Button onPress={onOpen}>Actionsheet</Button>
+          <Actionsheet isOpen={isOpen} onClose={onClose}>
+            <Actionsheet.Content>
+              <Box w="100%" h={60} px={4} justifyContent="center">
+                <Text
+                  fontSize="16"
+                  color="gray.500"
+                  _dark={{
+                    color: 'gray.300',
+                  }}>
+                  Albums
+                </Text>
+              </Box>
+              <Actionsheet.Item>Delete</Actionsheet.Item>
+              <Actionsheet.Item>Share</Actionsheet.Item>
+              <Actionsheet.Item>Play</Actionsheet.Item>
+              <Actionsheet.Item>Favourite</Actionsheet.Item>
+              <Actionsheet.Item>Cancel</Actionsheet.Item>
+            </Actionsheet.Content>
+          </Actionsheet>
+        </Center>
+
+        <FlatList
+          width="100%"
+          data={data}
+          renderItem={({item}) => (
+            <Box
+              borderBottomWidth="1"
               _dark={{
-                color: 'warmGray.50',
+                borderColor: 'gray.600',
               }}
-              color="coolGray.800"
-              bold>
-              {item.fullName}
-            </Text>
-          </Box>
-        )}
-        keyExtractor={item => item.id}
-      />
-    </Box>
+              borderColor="coolGray.200"
+              pl="4"
+              pr="5"
+              py="2">
+              <HStack space={3} justifyContent="space-between">
+                <Avatar
+                  size="48px"
+                  source={{
+                    uri: item.avatarUrl,
+                  }}
+                />
+                <VStack>
+                  <Text
+                    _dark={{
+                      color: 'warmGray.50',
+                    }}
+                    color="coolGray.800"
+                    bold>
+                    {item.fullName}
+                  </Text>
+                  <Text
+                    color="coolGray.600"
+                    _dark={{
+                      color: 'warmGray.200',
+                    }}>
+                    {item.recentText}
+                  </Text>
+                </VStack>
+                <Spacer />
+                <Text
+                  fontSize="xs"
+                  _dark={{
+                    color: 'warmGray.50',
+                  }}
+                  color="coolGray.800"
+                  alignSelf="flex-start">
+                  {item.timeStamp}
+                </Text>
+              </HStack>
+            </Box>
+          )}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    </NativeBaseProvider>
   )
 }
